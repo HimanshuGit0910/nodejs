f43a8d1af29eb280762fbcc957a6596d77f93012
himanshugit0910 <himanshugit0910@gmail.com>
2023-01-11 16:13:00 +0530

    all most

--- controllers/products.js
+++ controllers/products.js
@@ -1,13 +1,28 @@
 const Product=require("../models/products");
 
+
 const getAllProducts = async (req ,res)=>{
-    const myData= await Product.find({});
-    res.status(200).json({myData})
+//     const {company, name}=res.query;
+// const queryObject={};
+
+// if(company,name){
+//     queryObject.company=company;
+   // queryObject.name =name;
+// }
+// if(name){
+//     // queryObject.company=company;
+//     queryObject.name ={$regex:name,$options:"i"};
+// }
+
+    const  Himanshu = await Product.find(req.query);
+    res.status(200).json({Himanshu})
 };
 
+
 const getAllProductstesting = async  (req ,res)=>{
-    const myData= await Product.find({name:"samsung"});
-    res.status(200).json({myData})
+    const Himanshu= await Product.find(req.query);
+    console.log(req.query);
+    res.status(200).json({Himanshu})
 };
 
 module.exports={getAllProducts,getAllProductstesting};
--- productsdb.js
+++ productsdb.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const connectDB=require("./db/connects");
+const products = require("./models/products");
 const Product =require("./models/products");
 const productjson=require("./products.json");
 
@@ -7,6 +8,7 @@ const productjson=require("./products.json");
 const start =async()=>{
     try{
         await connectDB(process.env.Mongodb_Url);
+        await Product.deleteMany();
         console.log("sucesss");
         await Product.create(productjson);
 
